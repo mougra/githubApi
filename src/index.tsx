ac98fdb7d9e4e04f3f4acca6c46289c2c7a2b09a
egor antrop <mougraaa@gmail.com>
2022-12-23 01:35:08 +0400

    feat: search API & debounced

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
     <HashRouter>
-      {/* <BrowserRouter> */}
       <App />
     </HashRouter>
-    {/* </BrowserRouter> */}
   </Provider>
 )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import { useSearchUsersQuery } from '../store/github/github.api'
+import { useDebounce } from './../hooks/debounce'
 
 export function HomePage() {
-  const { isLoading, isError, data } = useSearchUsersQuery('vladilen')
+  const [search, setSearch] = useState('')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const {
+    isLoading,
+    isError,
+    data: users,
+  } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+    refetchOnFocus: true,
+  })
 
-  console.log(data)
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && users?.length! > 0)
+  }, [debounced, users])
 
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -16,11 +30,22 @@ export function HomePage() {
           type='text'
           className='border py-2 px-4 w-full h-[42px] mb-2'
           placeholder='Search for Github username...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <div className='ablosute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-          blanditiis!
-        </div>
+        {dropdown && (
+          <ul className='list-none ablosute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+            {isLoading && <p className='text-center'>Loading...</p>}
+            {users?.map((user) => (
+              <li
+                className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+                key={user.id}
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   )
